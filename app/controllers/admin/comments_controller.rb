f4d43ce0477364a868c9d3977ed787e12c519a7d
EC2 Default User <ec2-user@ip-172-31-25-44.ap-northeast-1.compute.internal>
2022-10-19 11:37:35 +0000

    [fix]design

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -4,6 +4,6 @@ class Admin::CommentsController < ApplicationController
     post    = Post.find(params[:post_id])
     comment = Comment.find_by(post_id: post.id)
     comment.destroy
-    redirect_to admin_members
+    redirect_back(fallback_location: root_url)
   end
 end
